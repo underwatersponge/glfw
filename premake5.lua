1c2ec4499a89a92808ed8fd21e2c06b436f4a76c
underwatersponge <2593214969@qq.com>
2025-12-17 22:01:53 +0800

    Modify premake5.lua for compile error

--- premake5.lua
+++ premake5.lua
@@ -12,35 +12,58 @@ project "GLFW"
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/context.c",
+        "src/internal.h",
         "src/init.c",
         "src/input.c",
+        "src/platform.h",
+        "src/platform.c",
+        "src/osmesa_context.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
+        "src/mappings.h",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
 
+    -- look the targetsource in CMakeLists.txt
     filter "system:windows"
         buildoptions {"-std=c11", "-lgdi32"}
         systemversion "latest"
         staticruntime "On"
         
+        defines 
+        { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+
+        }
+
         files
         {
             "src/win32_init.c",
             "src/win32_joystick.h",
+            "src/win32_joystick.c",
+            "src/win32_joystick.h",
             "src/win32_monitor.h",
+            "src/win32_module.c",
+            "src/win32_monitor.c",
+            "src/win32_platform.h",
             "src/win32_thread.h",
             "src/win32_thread.c",
+            "src/win32_time.h",
+            "src/win32_time.c",
             "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+            -- "src/egl_context.c",
         }
 
-        defines
-        {
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-        }
         filter {"system:widnows", "configurations:Release"}
             buildoptions "/MT"
